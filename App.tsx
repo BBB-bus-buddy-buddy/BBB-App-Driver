bca77922ba3d28ffed15fffc17471e80df3dfdad
jsum01 <djadjawltjr@gmail.com>
2025-04-24 23:50:49 +0900

    Feat: add import code to App.tsx (react-native-inappbrowser-reborn)

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 
 const App = () => {
   return (
